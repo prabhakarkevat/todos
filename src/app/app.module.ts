366684bd1d2265e3abf32d309ad658dffb2aed0e
Prabhakar Kevat <prabhakar.kevat@crossasyst.com>
2023-03-19 19:28:37 +0530

    added ngrx, effects, api gateway url to fetch the todos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
+import { StoreModule } from '@ngrx/store';
 import { AppComponent } from './app.component';
+import { MainComponent } from './components/main/main.component';
+import { TodoModule } from './components/todo/todo.module';
+import { todoReducer } from './state/todos/todo.reducer';
+import { EffectsModule } from '@ngrx/effects';
+import { TodoEffects } from './state/todos/todo.effects';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, MainComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    TodoModule,
+    HttpClientModule,
+    StoreModule.forRoot({ todos: todoReducer }),
+    EffectsModule.forRoot([TodoEffects]),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
